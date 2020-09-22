14f025e23b6e71dfae9bae3e546c03a7f346f07e
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2020-09-22 12:30:46 -0400

    Updated Test for Pruner to check if no_cache was called

--- test/mongo_pruner_test.py
+++ test/mongo_pruner_test.py
@@ -29,4 +29,4 @@ class MongoPrunerTest(unittest.TestCase):
         self.mongo_pruner._stop_event = Mock(wait=Mock(side_effect=[False, True]))
 
         self.mongo_pruner.run()
-        self.assertTrue(self.collection_mock.objects.return_value.delete.called)
+        self.assertTrue(self.collection_mock.objects.return_value.no_cache.called)
